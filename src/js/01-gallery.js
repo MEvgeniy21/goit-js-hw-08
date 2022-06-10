2217f0d2aba301875c4c937904ec5ec225764606
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-06-10 20:19:17 +0300

    task-1, task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+// lightbox.on('show.simplelightbox', function () {
+//    // do something…
+// });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const persistedVideoTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+const iframe = document.querySelector('iframe');
+const webPlayer = new Player(iframe);
+
+if (persistedVideoTime) {
+  setVideoTimeFromLS(JSON.parse(persistedVideoTime));
+}
+
+webPlayer.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  saveVideoTimeToLS(data.seconds);
+}
+
+function saveVideoTimeToLS(time) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(time));
+}
+
+function setVideoTimeFromLS(time) {
+  webPlayer.setCurrentTime(time);
+}
